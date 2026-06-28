a421ec7ceca663c6368581e1553a23a7bbd89f8a
TaeruAlethea <61478538+TaeruAlethea@users.noreply.github.com>
2026-06-28 02:23:37 -0400

    init Flake

---
+++ flake.nix
@@ -0,0 +1,41 @@
+{
+  description = "A Flake for developing Vintage Story (game) Mods";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+  };
+
+  outputs =
+    inputs@{ self, ... }:
+    inputs.flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [ "x86_64-linux" ];
+      perSystem =
+        {
+          pkgs,
+          lib,
+          system,
+          ...
+        }:
+        {
+          _module.args.pkgs = import self.inputs.nixpkgs {
+            inherit system;
+            config.allowUnfreePredicate =
+              pkg:
+              builtins.elem (lib.getName pkg) [
+                "vintagestory"
+              ];
+          };
+
+          devShells = {
+            default = pkgs.mkShell {
+              nativeBuildInputs = with pkgs; [
+                dotnetCorePackages.sdk_8_0
+              ];
+              DOTNET_BIN = "${pkgs.dotnetCorePackages.sdk_8_0}/bin/dotnet";
+              VINTAGE_STORY = "${pkgs.vintagestory}/share/vintagestory";
+            };
+          };
+        };
+    };
+}
