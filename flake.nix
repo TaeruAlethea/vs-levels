fd84ff9c2137733deb3c8311b9211f3808ac3e71
TaeruAlethea <61478538+TaeruAlethea@users.noreply.github.com>
2026-06-28 03:12:55 -0400

    Rider doesn't make it any better.

--- flake.nix
+++ flake.nix
@@ -24,24 +24,12 @@
               pkg:
               builtins.elem (lib.getName pkg) [
                 "vintagestory"
-                "rider"
               ];
           };
 
           devShells = {
             default = pkgs.mkShell {
-
               nativeBuildInputs = with pkgs; [ dotnetCorePackages.sdk_10_0 ];
-              env = {
-                DOTNET_BIN = "${pkgs.dotnetCorePackages.sdk_10_0}/bin/dotnet";
-                VINTAGE_STORY = "${pkgs.vintagestory}/share/vintagestory";
-              };
-            };
-            rider = pkgs.mkShell {
-              nativeBuildInputs = with pkgs; [
-                dotnetCorePackages.sdk_10_0
-                jetbrains.rider
-              ];
               env = {
                 DOTNET_BIN = "${pkgs.dotnetCorePackages.sdk_10_0}/bin/dotnet";
                 VINTAGE_STORY = "${pkgs.vintagestory}/share/vintagestory";
